0d3f767578249d9a37cd6b981e6805862ba8c9be
İlkan Atlı <atliilkan@gmail.com>
2023-09-12 23:27:55 +0300

    DevTools Aktif etmek

---
+++ src/main/java/com/ilkanatli/utils/ConfigProgramming.java
@@ -0,0 +1,11 @@
+package com.ilkanatli.utils;
+
+public class ConfigProgramming {
+
+    //    Setting ==> ctrl+alt+s
+    //    Alt+Ins ==> constructor, toString ,getter setter
+    //    Ctrl+/ => single comment
+    //    Ctrl+shift+/ => multiple comment
+    //    Ctrl+alt+L=formmater
+
+}
